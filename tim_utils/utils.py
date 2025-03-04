4ffdbc126c481f6f310aded7f841911ef75505b1
Matthew Nigh <mgnigh98@gmail.com>
2025-03-03 18:18:30 -0600

    Refactor SpecScaler class with improved error handling and tightening specs

--- tim_utils/utils.py
+++ tim_utils/utils.py
@@ -4,6 +4,9 @@ from . import *
 from copy import deepcopy
 
 from .kde import KernelDensity
+from .numpy import array
+
+import traceback
 
 bool = [False, True]
 
@@ -115,9 +118,32 @@ def act_func(activation):
     ])[activation]
 
 
-class SpecScaler:
-    def __init__(self, specs):
+class oldSpecScaler:
+    def __init__(self, specs, tighten_specs=False, tighten_method='absolute'):
         self.specs = specs
+        self.tighten_specs = tighten_specs
+        if tighten_method not in ['absolute', 'relative']:
+            raise NotImplementedError("tighten_method must be one of 'absolute', 'relative'")
+        else:
+            self.tighten_method = tighten_method
+
+    def _tighten_specs(self, lower, upper, col):
+        if self.tighten_method == 'absolute':
+            mean = np.mean([lower, upper])
+            range = upper-lower
+
+            lower = mean - self.tighten_specs*(range/2)
+            upper = mean + self.tighten_specs*(range/2)
+        elif self.tighten_method == 'relative':
+            print(type(self.data))
+            print(col)
+            data = self.data[col]
+            raise Exception
+            lower = np.quantile(data[(data>lower) & (data<upper)], (1-self.tighten_specs)/2)
+            upper = np.quantile(data[(data>lower) & (data<upper)], 1-((1-self.tighten_specs)/2))
+        return lower, upper
+        
+        
 
     def _upper_lower(self, column):
         mask_ = self.specs[self.specs.iloc[:, 0].apply(lambda x: x in column)]
@@ -126,8 +152,15 @@ class SpecScaler:
             lower = mask[[col for col in mask.columns if "low" in col.lower()][0]].values.item()
             upper = mask[
                 [col for col in mask.columns if ("high" in col.lower()) | ("up" in col.lower())][0]].values.item()
-        except:
-            print(f"Warning: column '{column}' not found in specification file, ignore if not expected to scale column")
+            if self.tighten_specs:
+                lower, upper  = self._tighten_specs(lower, upper, column)
+        except Exception as e:
+            if str(e) == 'attempt to get argmax of an empty sequence':
+                print(f"Warning: column '{column}' not found in specification file, ignore if not expected to scale column")
+            else:
+                print("EXCEPTION")
+                print(e)
+                traceback.print_exc()
             upper, lower = np.NaN, np.NaN
         return upper, lower
 
@@ -186,6 +219,106 @@ class SpecScaler:
                 stats.loc[stats.index[i], "UpperLimit":"Min"] = upper, lower, data[col].max(), data[col].min()
                 above, below = np.sum(data[col]>upper), np.sum(data[col]<lower)
                 stats.loc[stats.index[i], "Above":"Within"] = above ,below ,total_count-(above+below)
+        return stats
+
+class SpecScaler:
+    def __init__(self, specs, tighten_specs=False, tighten_method='absolute'):
+        self.specs = specs
+        self.tighten_specs = tighten_specs
+        if tighten_method not in ['absolute', 'relative']:
+            raise NotImplementedError("tighten_method must be one of 'absolute', 'relative'")
+        else:
+            self.tighten_method = tighten_method
+
+    def _tighten_specs(self, lower, upper, col):
+        if self.tighten_method == 'absolute':
+            mean = np.mean([lower, upper])
+            range = upper-lower
+
+            lower = mean - self.tighten_specs*(range/2)
+            upper = mean + self.tighten_specs*(range/2)
+        elif self.tighten_method == 'relative':
+            data = self.data[col]
+            lower = np.quantile(data[(data>lower) & (data<upper)], (1-self.tighten_specs)/2)
+            upper = np.quantile(data[(data>lower) & (data<upper)], 1-((1-self.tighten_specs)/2))
+        return lower, upper
+        
+
+    def _upper_lower(self, column):
+        mask_ = self.specs[self.specs.iloc[:, 0].apply(lambda x: x in column)]
+        try:
+            mask = mask_.iloc[[mask_.iloc[:, 0].str.len().argmax()]]
+            lower = mask[[col for col in mask.columns if "low" in col.lower()][0]].values.item()
+            upper = mask[
+                [col for col in mask.columns if ("high" in col.lower()) | ("up" in col.lower())][0]].values.item()
+            if self.tighten_specs:
+                lower_, upper_  = self._tighten_specs(lower, upper, column)
+            else:
+                lower_, upper_ = lower, upper
+        except Exception as e:
+            if str(e) == 'attempt to get argmax of an empty sequence':
+                print(f"Warning: column '{column}' not found in specification file, ignore if not expected to scale column")
+            else:
+                print("EXCEPTION")
+                print(e)
+            upper_, lower_ = np.nan, np.nan
+        return upper_, lower_
+
+    def upper_lower_limits(self, columns):
+        columns = [columns] if isinstance(columns, str) else columns
+        lowers, uppers = np.array([]), np.array([])
+        for col in columns:
+            upper, lower = self._upper_lower(col)
+            lowers = np.append(lowers, lower)
+            uppers = np.append(uppers, upper)
+        return uppers, lowers
+
+    def transform(self, data):
+        self.data = data
+        scaled = pd.DataFrame(columns=self.data.columns)
+        for col in self.data.columns:
+            data_col = self.data[col]
+            upper, lower = self._upper_lower(col)
+            if np.isnan(upper):
+                scaled[col] = self.data[col]
+            else:
+                scaled[col] = (data_col - lower) / (upper-lower)
+        return scaled
+    fit_transform = transform
+
+    def inverse_transform(self, scaled):
+        try:
+            if "pandas" in str(type(scaled)):
+                in_scaled = pd.DataFrame(columns=scaled.columns)
+            else:
+                in_scaled = pd.DataFrame(columns=self.data.columns)
+        except:
+            raise Exception("'transform' was not called previously and input to 'inverse_transform' did not contain column labels")
+
+        for i,col in enumerate(self.data.columns):
+            upper, lower = self._upper_lower(col)
+            if np.isnan(upper):
+                in_scaled[col] = scaled[col]
+            elif "numpy" in str(type(scaled)):
+                in_scaled[col] = scaled[:,i]*(upper-lower)+lower
+            elif "pandas" in str(type(scaled)):
+                in_scaled[col] = scaled[col]*(upper-lower)+lower
+            else:
+                raise Exception("Ensure to use Pandas DataFrame or Numpy Array as input")
+        return in_scaled
+
+    def return_stats(self, data):
+        stats = pd.DataFrame(columns=["Parameter", "Above", "Below", "Within", "UpperLimit", "LowerLimit", "Max", "Min"])
+        stats["Parameter"] = data.columns
+        total_count = data.shape[0]
+        for i,col in enumerate(data.columns[data.columns.isin(self.data.columns)]):
+            upper, lower = self._upper_lower(col)
+            if np.isnan(upper):
+                stats.loc[stats["Parameter"]==col, 1:-2] = np.NaN
+            else:
+                stats.loc[stats["Parameter"]==col, "UpperLimit":"Min"] = upper, lower, data[col].max(), data[col].min()
+                above, below = np.sum(data[col]>upper), np.sum(data[col]<lower)
+                stats.loc[stats["Parameter"]==col, "Above":"Within"] = above ,below ,total_count-(above+below)
         return stats
 
 
