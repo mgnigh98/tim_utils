1ee9ee9bba83e83641ef4dc176cd7565feb327a8
Matthew Nigh <mgnigh98@gmail.com>
2025-04-10 16:49:26 -0500

    Add numpy.py with commented-out class definition for custom ndarray

---
+++ tim_utils/numpy.py
@@ -0,0 +1,8 @@
+#this does not work because of the way numpy is implemented with cython
+
+# import numpy
+
+# class array(numpy.ndarray):
+#     @property
+#     def N(self):
+#         return self[..., None]
